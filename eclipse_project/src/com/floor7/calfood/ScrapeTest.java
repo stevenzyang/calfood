6a79d6c1d2d3a2d8ced6cef9bb631ad2c64906b4
Steven Yang <stevenzyang@berkeley.edu>
2013-02-09 20:28:47 -0800

    weird bug

--- eclipse_project/src/com/floor7/calfood/ScrapeTest.java
+++ eclipse_project/src/com/floor7/calfood/ScrapeTest.java
@@ -11,20 +11,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.io.*;
 
 public class ScrapeTest {
 	public static void main(String[] args) {
 		int days_in_advance = 16;
 		int total_num_of_foods = 0;
-		HashSet<String> Crossroads = new HashSet<String>();
-		HashSet<String> Cafe_3 = new HashSet<String>();
-		HashSet<String> Clark_Kerr = new HashSet<String>();
-		HashSet<String> Foothill = new HashSet<String>();
-		HashSet<String> allfoods = new HashSet<String>();
-		ArrayList<Food> foods = new ArrayList<Food>();
+		//boolean isBreakfast = false;
+		//boolean isLunch = false;
+		//boolean isDinner = false;
+		HashSet<Food> foods = new HashSet<Food>();
 		Calendar cal = Calendar.getInstance();
 		Pattern foodpattern = Pattern.compile("openDescWin\\('','(.*?)'\\)");
+		//Pattern breakfast = Pattern.compile("");
 		String URL1 = "http://services.housing.berkeley.edu/FoodPro/dining/static/diningmenus.asp?dtCurDate=";
 		String URL2 = "&strCurLocation=";
 		String date = (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.YEAR);
@@ -38,48 +36,22 @@ public class ScrapeTest {
 					String URL = URL1 + date + URL2 + location[j];
 					Document doc = Jsoup.connect(URL).get();
 					Element body = doc.body();
+					//isBreakfast = true;
 					Matcher m = foodpattern.matcher(body.html());
 					while (m.find()) {
 						total_num_of_foods += 1;
 						String s = m.group(1);
-						switch(j) {
-							case 0:
-								Crossroads.add(s);
-								break;
-							case 1:
-								Cafe_3.add(s);
-								break;
-							case 2:
-								Clark_Kerr.add(s);
-								break;
-							case 3:
-								Foothill.add(s);
-								break;
-							default:
-								break;
-						}
+						foods.add(new Food(s));
 					}
 				}
 				today = today.getTomorrow();
 				date = today.toString();
 			}
-			for (String s : Crossroads) {
-				out1.println(s);
-			}
-			for (String s : Cafe_3) {
-				out2.println(s);
-			}
-			for (String s : Clark_Kerr) {
-				out3.println(s);
-			}
-			for (String s : Foothill) {
-				out4.println(s);
+			for (Food f : foods) {
+				out.println(f);
 			}
 			System.out.println("Found " + total_num_of_foods + " foods");
-			out1.close();
-			out2.close();
-			out3.close();
-			out4.close();
+			out.close();
 		} catch (IOException e) {
 			System.err.println("Error");
 		}
@@ -90,14 +62,35 @@ class Food {
 	public String name;
 	public ArrayList<FoodCoordinate> appearances = new ArrayList<FoodCoordinate>();
 	public int rating = 0;
-	
+
+	public Food(String n) {
+		name = n;
+	}
+
 	void addCoordinate(String l, Date d, String t) {
 		appearances.add(new FoodCoordinate(l, d, t));
 	}
-	
-//	@Override
-//	public boolean equals(Object obj) {
-//	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this.getClass() != obj.getClass()) {
+			return false;
+		}
+		if (this.toString().equals(((Food) obj).toString())) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
 }
 
 
@@ -105,24 +98,22 @@ class FoodCoordinate {
 	private String location;
 	private Date date;
 	private String time;
-	
+
 	FoodCoordinate(String l, Date d, String t) {
 		location = l;
 		date = d;
 		time = t;
 	}
-	
+
 	String getLocation() {
 		return location;
 	}
-	
+
 	Date getDate() {
 		return date;
 	}
-	
 
 	String getTime() {
 		return time;
 	}
-
 }
