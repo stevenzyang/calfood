22b3121ddb359fb293d1366e4db03b6fcd2c3566
skiinnout <khsieh@berkeley.edu>
2013-02-10 14:38:37 -0800

    changed hashset storage of foods to hashtable

--- eclipse_project/src/com/floor7/calfood/ScrapeTest.java
+++ eclipse_project/src/com/floor7/calfood/ScrapeTest.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Calendar;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -20,19 +21,21 @@ public class ScrapeTest {
 	public static final String FH = "06";
 	
 	public static void main(String[] args) throws IOException{
-		ArrayList<Food> foods = getFoods(CK);
+		/*ArrayList<Food> foods = getFoods(CK);
 		for (Food food : foods){
 			System.out.println(food);
-		}
+		} */
+		updateFoods();
 	}
 	
 	public static void updateFoods() {
+		int testoverlap = 0;
 		int days_in_advance = 16;
 		int total_num_of_foods = 0;
 		//boolean isBreakfast = false;
 		//boolean isLunch = false;
 		//boolean isDinner = false;
-		HashSet<Food> foods = new HashSet<Food>();
+		Hashtable<String, Food> foodtable = new Hashtable<String, Food>();
 		//Pattern breakfast = Pattern.compile("");
 		Calendar cal = Calendar.getInstance();
 		String date = (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.YEAR);
@@ -49,16 +52,32 @@ public class ScrapeTest {
 					while (m.find()) {
 						total_num_of_foods += 1;
 						String s = m.group(1);
-						foods.add(new Food(s));
+						if (foodtable.containsKey(s)) {
+							testoverlap++;
+							foodtable.get(s).addCoordinate(location[j], today, "All Day");
+						} else {
+							Food temp = new Food(s);
+							temp.addCoordinate(location[j], today, "All Day");
+							foodtable.put(s, temp);
+						}
 					}
 				}
 				today = today.getTomorrow();
 				date = today.toString();
 			}
-			for (Food f : foods) {
+			for (Food f : foodtable.values()) {
 				out.println(f);
+				for (FoodCoordinate fc : f.appearances) {
+					if (fc.getLocation().equals("Cafe 3")) {
+						out.println("\t" + fc.getLocation() + "\t\t" + fc.getDate() + "\t" + fc.getTime());
+					} else {
+						out.println("\t" + fc.getLocation() + "\t" + fc.getDate() + "\t" + fc.getTime());
+					}
+				}
+				out.println();
 			}
 			System.out.println("Found " + total_num_of_foods + " foods");
+			System.out.println("Had " + testoverlap + " overlaps.");
 			out.close();
 		} catch (IOException e) {
 			System.err.println("Error");
@@ -69,7 +88,7 @@ public class ScrapeTest {
 	 * 			ScrapeTest.XR, ScrapeTest.C3, ScrapeTest.CK, and ScrapeTest.FH
 	 * @return the list of foods from the location
 	 */
-	public static ArrayList<Food> getFoods(String loc) throws IOException{
+	public static ArrayList<Food> getFoods(String loc) throws IOException {
 		Calendar cal = Calendar.getInstance();
 		String date = (cal.get(Calendar.MONTH) + 1) + "/" + cal.get(Calendar.DAY_OF_MONTH) + "/" + cal.get(Calendar.YEAR);
 		Date today = new Date(date);
@@ -82,8 +101,19 @@ public class ScrapeTest {
 		}
 		return foods;
 	}
+
+	public static ArrayList<Food> getFoods(String loc, Date date) throws IOException {
+		String d = date.toString();
+		String URL = getURL(loc, d);
+		Matcher m = matchPattern("openDescWin\\('','(.*?)'\\)", URL);
+		ArrayList<Food> foods = new ArrayList<Food>(15);
+		while (m.find()) {
+			foods.add(new Food(m.group(1)));
+		}
+		return foods;
+	}
 	
-	private static Matcher matchPattern(String regex, String URL) throws IOException{
+	private static Matcher matchPattern(String regex, String URL) throws IOException {
 		Pattern foodpattern = Pattern.compile(regex);
 		Document doc = Jsoup.connect(URL).get();
 		Element body = doc.body();
@@ -145,7 +175,15 @@ class FoodCoordinate {
 	private String time;
 
 	FoodCoordinate(String l, Date d, String t) {
-		location = l;
+		if (l.equals("01")) {
+			location = "Crossroads";
+		} else if (l.equals("03")) {
+			location = "Cafe 3";
+		} else if (l.equals("04")) {
+			location = "Clark Kerr";
+		} else {
+			location = "Foothill";
+		}
 		date = d;
 		time = t;
 	}
