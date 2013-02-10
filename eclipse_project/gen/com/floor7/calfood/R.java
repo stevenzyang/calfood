b78e5beadfc836c40130fffdf6d094d9466dbd15
Steven Yang <stevenzyang@berkeley.edu>
2013-02-09 17:09:27 -0800

    cause it said so

--- eclipse_project/gen/com/floor7/calfood/R.java
+++ eclipse_project/gen/com/floor7/calfood/R.java
@@ -15,7 +15,8 @@ public final class R {
     }
     public static final class id {
         public static final int container=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int menu_settings=0x7f070002;
+        public static final int my_button=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -26,11 +27,9 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040004;
-        public static final int menu_settings=0x7f040005;
-        public static final int title_section1=0x7f040003;
-        public static final int title_section2=0x7f040002;
-        public static final int title_section3=0x7f040001;
+        public static final int hello_world=0x7f040001;
+        public static final int menu_settings=0x7f040002;
+        public static final int my_button_text=0x7f040003;
     }
     public static final class style {
         /** 
